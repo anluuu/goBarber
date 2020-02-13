5eb932db4d9299f1f785b1ca1d0d83800c14a009
anlu <luancunha12@gmail.com>
2020-02-13 11:51:14 -0300

    Initial commit

---
+++ src/app/models/Recipient.js
@@ -0,0 +1,11 @@
+import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
+
+class Recipient extends Model {
+  static init(sequelize) {
+    super.init({
+
+    })
+  }
+
+}
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+    secret: 'b7aa7530dbb64e2488239109ba30594c',
+    expiresIn: '7d',
+}
